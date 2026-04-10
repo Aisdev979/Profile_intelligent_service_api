e021f8993bd5dff3afcd33987e1f377c3f80f4b6
Ogbebor Aisosa Matthew <aisdevinstitute@gmail.com>
2026-04-10 21:31:01 +0100

    added cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
 import errorMiddleware from "./middlewares/error.middlewares.js"
 import genderPredictionRoute from "./routes/genderPrediction.routes.js"
 
 const app = express();
 dotenv.config()
+
+app.use(cors());
 
 app.use("/", genderPredictionRoute);
 
